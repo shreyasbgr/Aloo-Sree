ced4fc9559eeea49b6f6cedb2ad547a274d9af80
Shreyas77 <shreyasbanagar7@gmail.com>
2018-12-12 16:41:40 +0530

    Adding the game files

---
+++ android/app/src/main/java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "Tyr8cpiusApPLf4jyKfhWhYvqnTJrrwPGXj7cs6n4AgXdvF0zf2xCxZ2qXXK+uNcTCqpJ8+qs1tNdvt3yfnjUA==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
